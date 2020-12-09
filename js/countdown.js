5812b08de3b9eb8263ae0e1eb8987ba9e0cf047a
Joseph Cortez <52973770+cortez@users.noreply.github.com>
2020-12-08 18:37:45 -0600

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,15 +1,23 @@
 var countDownDate = new Date("Dec 25, 2020 00:00:00").getTime();
 
-    var x = setInterval(function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "0";
+    }
+
+    var lessthan = days.toString();
+    var lessthanten = lessthan.substring(1);
+
+    if (days < 10) {
+        console.log(lessthanten);
+        document.getElementById("countdown").innerHTML = lessthanten;
+    } else {
         document.getElementById("countdown").innerHTML = days;
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "0";
-        }
-    }, 1000);
+    }
+
+}, 1000);
