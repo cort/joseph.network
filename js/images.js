f165427d8a6cd7f9fbf71ca78c449d818fde642d
Joseph Cortez <52973770+cortez@users.noreply.github.com>
2020-12-17 21:10:39 -0600

    Add files via upload

---
+++ js/images.js
@@ -0,0 +1,26 @@
+
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.chaincore'),
+    intensity: 0.5,
+    image1: 'img/byjlc1.png',
+    image2: 'img/byjlc2.png',
+    displacementImage: 'js/4.png'
+});
+
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.worldplaces'),
+    intensity: 0.5,
+    image1: 'img/worldplaces1.jpg',
+    image2: 'img/worldplaces2.jpg',
+    displacementImage: 'js/9.jpg'
+});
+
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.unlikely'),
+    intensity: 0.5,
+    image1: 'img/unlikely1.png',
+    image2: 'img/unlikely2.png',
+    displacementImage: 'js/14.jpg'
+});
+
+// duplicate the above block with a new class name when making a new post i.e. '.post4'
