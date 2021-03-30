d9fb8ea5d1675e1c2acfcdd6c9db9661b6da7f7c
Joseph Cortez <52973770+cortez@users.noreply.github.com>
2021-03-29 19:12:15 -0500

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,31 +1,31 @@
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.covid'),
     intensity: 0.5,
-    image1: 'img/covid1.png',
-    image2: 'img/covid2.png',
+    image1: 'img/covidCover.png',
+    image2: 'img/covid1.png',
     displacementImage: 'js/8.jpg'
 });
 
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.worldplaces'),
     intensity: 0.5,
-    image1: 'img/worldplaces1.jpg',
-    image2: 'img/worldplaces2.jpg',
+    image1: 'img/worldplacesCover.jpg',
+    image2: 'img/worldplaces1.jpg',
     displacementImage: 'js/4.png'
 });
 
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.unlikely'),
     intensity: 0.5,
-    image1: 'img/unlikely1.png',
-    image2: 'img/unlikely2.png',
+    image1: 'img/unlikelyCover.png',
+    image2: 'img/unlikely1.png',
     displacementImage: 'js/14.jpg'
 });
 
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.photography'),
     intensity: 0.5,
-    image1: 'img/photos1.png',
+    image1: 'img/photographyCover.png',
     image2: 'img/photos2.jpg',
     displacementImage: 'js/9.jpg'
 });
